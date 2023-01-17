04d3797dcedd81fed9d18889abb27c3d7622cd84
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-01-17 17:40:08 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+let array = [0];
 
 function App() {
+  const [count, setCount] = useState(1);
+
+
+  const handleClick = () => {
+    setCount(count + 1);
+
+    array.push(" ")
+    array.push(count)
+  }
+
+  const handleDecrement = ()=>{
+    setCount(count - 1);
+    array.pop()
+    array.pop()
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       
+      <h1>{array}</h1>
+      <div className="button__wrapper">
+        <button onClick={handleClick}>Increment</button>
+        <button onClick={handleDecrement} disabled={count===1} >Decrement</button>
+      </div>
     </div>
   );
 }
